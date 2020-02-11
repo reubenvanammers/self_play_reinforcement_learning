ec918c5941475ed0b9ef15b25ecb4015dbcea67a
Reuben van Ammers <reuben.vanammers@eliiza.com.au>
2020-02-12 10:09:27 +1100

    first attempt at getting multiple outputs to work, and updated some self play stuff

--- games/connect4/q.py
+++ games/connect4/q.py
@@ -13,6 +13,9 @@ class Memory:
     def __init__(self, max_size):
         self.buffer = deque(maxlen=max_size)
 
+    def __len__(self):
+        return len(self.memory)
+
     def add(self, experience):
         self.buffer.append(experience)
 
@@ -37,47 +40,68 @@ class EpsilonGreedy:
 
 
 class QLinear(nn.Module):
-    def __call__(self, s, a):
+    def __call__(self, s):
         if not isinstance(s, torch.Tensor):
             # s = torch.scalar_tensor(s).long()
             s = torch.from_numpy(s).long()
-        if not isinstance(a, torch.Tensor):
-            a = torch.scalar_tensor(a).long()
-        return super().__call__(s, a)
+        # if not isinstance(a, torch.Tensor):
+        #     a = torch.scalar_tensor(a).long()
+        return super().__call__(s)
 
     def __init__(self, env, lr=0.025, gamma=1, momentum=0, buffer_size=0, batch_size=1):
         super().__init__()
 
         self.gamma = gamma
         self.env = env
-        self.linear = nn.Linear(self.env.width * self.env.width * self.env.height, 1)
+        self.linear = nn.Linear(self.env.width * self.env.width * self.env.height, self.env.action_space.n)
         self.linear.weight.data.fill_(0.5)
         self.optim = torch.optim.SGD(self.parameters(), momentum=momentum, lr=lr)
         if buffer_size:
             self.memory = Memory(buffer_size)
         self.batch_size = batch_size
 
-    def forward(self, s, a):  # At the moment just use a linear network - dont expect it to be good
-        a = nn.functional.one_hot(a, self.env.width)
-        # s = nn.functional.one_hot(s, output_size)
+    def forward(self, s):  # At the moment just use a linear network - dont expect it to be good
+        # a = nn.functional.one_hot(a, self.env.width)
+        # # s = nn.functional.one_hot(s, output_size)
         s = s.view(-1)
-        x = torch.functional.einsum("i,j->ij", s, a).float().view(-1)  # Cartesian product
+        # x = torch.functional.einsum("i,j->ij", s, a).float().view(-1)  # Cartesian product
 
-        return self.linear(x)
+        return self.linear(s)
+
+    def v(self, s, a):
+        return self(s).gather(1, a)
 
     def update(self, s, a, r, done, s_next):
+        s = torch.tensor(s)
+        a = torch.tensor(a)
+        r = torch.tensor(r)
+        s_next = torch.tensor(s_next)
+        if len(self.memory < self.batch_size):
+            self.memory.add((s, a, r, done, s_next))
+
         # prediction
         if self.memory:
             self.memory.add((s, a, r, done, s_next))
             batch = self.memory.sample(self.batch_size)
-        q = self(s, a)
+            batch_t = tuple(*zip(*batch))  # transposed batch
+        # q = self(s, a)
+
+        # prediction
+        s_batch, a_batch, r_batch, done_batch, s_next_batch = batch_t
+        q = self.v(s_batch, a_batch)
+        # seperate state,actions
 
         # actual
-        q_next = max(self(s_next, a_) for a_ in range(self.env.action_space.n))  # Check!
-        q_target = r + (1 - done) * self.gamma * q_next
+        q_next = self(s_batch).max(1)[0].detach()  # check how detach works (might be dodgy???)
+        # q_next = max(self(s_next, a_) for a_ in range(self.env.action_space.n))  # Check!
+        q_next_actual = (1 - done_batch) * q_next  # Removes elements that are done
+        q_target = r_batch + self.gamma * q_next_actual
+        # q_target = r + (1 - done) * self.gamma * q_next
         #         q_target = torch.scalar_tensor(q_target).unsqueeze(0)
         loss = F.smooth_l1_loss(q, q_target)
 
         self.optim.zero_grad()
         loss.backward()
+        for param in self.policy_net.parameters(): # see if this ends up doing anything - should just be relu
+            param.grad.data.clamp_(-1, 1)
         self.optim.step()
--- games/connect4/self_play.py
+++ games/connect4/self_play.py
@@ -6,7 +6,7 @@ import numpy
 class SelfPlay:
     # Given a learning policy, opponent policy , learns by playing opponent and then updating opponents model
     # TODO add function for alternating who starts first
-    def __init__(self, policy, opposing_policy):
+    def __init__(self, policy, opposing_policy, swap_sides=False):
         self.policy = policy
         self.opposing_policy = opposing_policy
         self.alternate_start = False
@@ -16,15 +16,18 @@ class SelfPlay:
 
         self.policy_wins = 0
         self.opponent_wins = 0
+        self.swap_sides = swap_sides
 
     def train_model(self, num_episodes):
         for episode in range(num_episodes):
             if episode % self.update_lag == 0 and episode > 0:
                 self.update_opponent_model()
-            self.play_episode()
+            self.play_episode(swap_sides=(self.swap_sides and episode % 2 == 0))
 
-    def play_episode(self):
+    def play_episode(self, swap_sides=False):
         s = self.env.reset()
+        if swap_sides:
+            s, _, _, _, _ = self.get_and_play_moves(s, player=-1)
         for i in range(100):  # Should be less than this
             s, done = self.play_round(s)
             if done:
@@ -34,16 +37,24 @@ class SelfPlay:
         # Make state as opposing policy will see it
         return s * -1
 
+    def get_and_play_moves(self, s, player=1):
+        if player == 1:
+            a = self.policy(s)
+            s_next, r, done, info = self.play_move(a, player=1)
+            return s_next, a, r, done, info
+        else:
+            opp_s = self.swap_state(s)
+            a = self.opposing_policy(opp_s)
+            s_next, r, done, info = self.play_move(a, player=-1)
+            return s_next, a, r, done, info
+
     def play_round(self, s):
-        a = self.policy(s)
-        s_next, r, done, info = self.play_move(a, player=1)
+        s_next, r, a, done, info = self.get_and_play_moves(s)
         if done:
             self.policy_wins += 1
             self.q.update(s, a, r, done, s_next)
         else:
-            s = self.swap_state(s_next)
-            a = self.opposing_policy(s)
-            s_next, r, done, info = self.play_move(a, player=-1)
+            s_next, r, a, done, info = self.get_and_play_moves(s_next, player=-1)
             if done:
                 self.opponent_wins += 1
             self.q.update(s, a, r, done, s_next)
