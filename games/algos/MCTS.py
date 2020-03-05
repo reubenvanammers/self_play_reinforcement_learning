9c6d26cfdff1287c4f6aed0d73992140bb57fc7f
Reuben van Ammers <reuben.vanammers@eliiza.com.au>
2020-03-05 12:41:22 +1100

    updated mcts stuff

--- games/algos/MCTS.py
+++ games/algos/MCTS.py
@@ -3,8 +3,7 @@ from anytree import NodeMixin
 from rl_utils.memory import Memory
 from collections import namedtuple
 
-Transition = namedtuple("Transition", ("state", "action", "reward", "done", "next_state")) #TODO - make transition relevant to MCTS
-
+Move = namedtuple("Move", ("state", "action", "predicted_val", "actual_val", "network_probs", "tree_probs"))
 
 
 class MCNode(NodeMixin):
@@ -17,6 +16,8 @@ class MCNode(NodeMixin):
         self.parent = parent
 
         self.cpuct = 1
+
+        self.v = None
 
     @property
     def q(self):
@@ -38,10 +39,6 @@ class MCNode(NodeMixin):
 
     def play_prob(self, temp):
         return np.power(self.n, 1 / temp)
-
-    # def prune(self, action = None):
-    #     # Only keep the specified action: delete the ot
-    #     if action:
 
     def create_children(self, action_probs):
         children_list = []
@@ -66,7 +63,10 @@ class MCTreeSearch:
         base_state = self.env.reset()
         self.root_node = MCNode(state=base_state)
 
+        # self.initial_probs, self.v = self.evaluator(base_state)
+
         self.temp_memory = []
+        self.memory = Memory()
 
         self.temperature_cutoff = temperature_cutoff
         self.actions = actions
@@ -74,40 +74,56 @@ class MCTreeSearch:
 
     def search_and_play(self):
         temperature = 1 if self.moves_played < self.temperature_cutoff else 0
+        # self.initial_probs, self.v = self.evaluator(base_state)
         self.search()
         move = self.play(temperature)
         return move
 
     def update(self, s, a, r, done, next_s):
-        #TODO - have temperorary queue and push to real queue when games are complete
-        if not done:
-            self.
+        if done:
+            for experience in self.temp_memory:
+                experience.actual_val = r
+                self.memory.add(experience)
+            self.temp_memory = []
+        # TODO atm start of with running update, then maybe move to async model like in paper
 
     def play(self, temp=0.01):
         play_probs = [child.play_prob(temp) for child in self.root_node.children]
+        move_probs = [child.p for child in self.root_node.children]
+
         choice = np.random.choice(self.actions, p=play_probs)
         self.root_node.children = self.root_node.children[choice]
         self.root_node = self.root_node.children[0]
 
         self.moves_played += 1
+
+        self.temp_memory.append(Move(self.root_node.state, choice, self.root_node.v, None, move_probs, play_probs))
         return choice
 
-    def expand_node(self, node, choice, player=1):
-        parent_state = node.parent.state
+    def expand_node(self, parent_node, choice, player=1):
         env = self.env_gen()
-        env.set_state(parent_state)
+        env.set_state(parent_node.state)
         s, r, done, _ = env.step(choice, player=player)
-        probs, v = self.evaluator(s)
-        node.create_children(probs)
-        return v
+        if done:
+            v = r
+            child_node = parent_node.children[choice]
+        else:
+            # TODO add if done set MCTS to appropriate val
+            probs, v = self.evaluator(s)
+            child_node = parent_node.children[choice]
+            child_node.create_children(probs)
+        return child_node, v
 
-
-def search(self):
-    node = self.root_node
-    for i in range(self.iterations):
-        while not node.is_leaf:
-            select_probs = [child.select_prob for child in node.children]
-            choice = np.random.choice(self.actions, p=select_probs)
-            node = node.children[choice]
-        v = self.expand_node(node, choice)
-        node.backup(v)
+    def search(self):
+        node = self.root_node
+        for i in range(self.iterations):
+            while True:
+                select_probs = [child.select_prob for child in node.children]
+                choice = np.random.choice(self.actions, p=select_probs)
+                if node.is_leaf:
+                    node, v = self.expand_node(node, choice)
+                    node.backup(v)
+                    node.v = v
+                    break
+                else:
+                    node = node.children[choice]
