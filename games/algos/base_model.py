a93560fe6c6cc47b10a54e2c0e079ded451c4bbc
Reuben van Ammers <reuben.vanammers@eliiza.com.au>
2020-07-12 13:20:08 +1000

    started work on elo and packaging models

--- games/algos/base_model.py
+++ games/algos/base_model.py
@@ -53,3 +53,15 @@ class BaseModel:
 
     def play_action(self, a):
         raise NotImplementedError
+
+
+class ModelContainer():
+
+    def __init__(self, policy_gen, policy_args, policy_kwargs):
+        self.policy_gen = policy_gen
+        self.policy_args = policy_args
+        self.policy_kwargs = policy_kwargs
+
+
+    def setup(self, **kwargs):
+        return self.policy_gen(*self.policy_args,**self.policy_kwargs,**kwargs)
---
+++ games/algos/elo.py
@@ -0,0 +1,66 @@
+from games.algos import self_play_parallel
+from games.connect4.connect4env import Connect4Env
+import shelve
+import os
+import itertools
+import atexit
+
+class Elo():
+    MODEL_SAVE_FILE = ".ELO_MODEL"
+    RESULT_SAVE_FILE = ".ELO_RESULT"
+
+    def __init___(self, env=Connect4Env):
+        path = os.path.dirname(__file__)
+        self.model_shelf = shelve.open(os.path.join(path, self.MODEL_SAVE_FILE))
+        self.result_shelf = shelve.open(os.path.join(path, self.RESULT_SAVE_FILE))
+
+        self.env = env
+
+    @atexit.register
+    def _close(self):
+        self.model_shelf.close()
+        self.result_shelf.close()
+
+    def add_model(self, name, policy_gen, policy_args, policy_kwargs):
+        if self.model_shelf[name]:
+            raise ValueError("Model name already in use")
+        self.model_shelf[name] = {
+            "policy_gen": policy_gen,
+            "policy_args": policy_args,
+            "policy_kwargs": policy_kwargs
+        }
+
+
+    def compare_models(self, *args):
+        combinations = itertools.combinations(args, 2)
+
+        for model_1, model_2 in combinations:
+            self._compare(model_1, model_2)
+
+
+    def _compare(self, model_1, model_2, num_games=100):
+        assert model_1 != model_2
+        assert "_" not in model_1
+        assert "_" not in model_2
+        if model_1 > model_2:
+            key = f"{model_1}__{model_2}"
+        else:
+            key = f"{model_2}__{model_1}"
+        if key in self.result_shelf:
+            old_results = self.result_shelf[key]
+        else:
+            old_results = {"wins": 0, "draws": 0, "losses": 0}
+        new_results = self._get_results(model_1, model_2)
+        total_results = {status: new_results[status] + old_results[status] for status in ("wins", "draws", "losses")}
+        self.result_shelf[key] = total_results
+
+    def _get_results(self, model_1, model_2, num_games=100):
+        model_1_dict = self.model_shelf[model_1]
+        model_2_dict = self.model_shelf[model_2]
+
+
+
+        scheduler = self_play_parallel.SelfPlayScheduler(mod)
+
+    def calculate_elo(self):
+        pass
