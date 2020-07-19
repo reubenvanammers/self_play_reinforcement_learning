5f3d5ce429963af8c1bf96d308a42a42bf795773
Reuben van Ammers <reuben.vanammers@eliiza.com.au>
2020-07-19 21:15:04 +1000

    cleanup and got persisten elo working

--- games/algos/elo.py
+++ games/algos/elo.py
@@ -19,11 +19,13 @@ result_container = namedtuple("result", ["players", "result"])
 class Elo():
     MODEL_SAVE_FILE = ".ELO_MODEL"
     RESULT_SAVE_FILE = ".ELO_RESULT"
+    ELO_VALUE_SAVE_FILE = ".ELO_VALUE"
 
     def __init__(self, env=Connect4Env):
         path = os.path.dirname(__file__)
         self.model_shelf = shelve.open(os.path.join(path, self.MODEL_SAVE_FILE))
         self.result_shelf = shelve.open(os.path.join(path, self.RESULT_SAVE_FILE))
+        self.elo_value_shelf = shelve.open(os.path.join(path, self.ELO_VALUE_SAVE_FILE))
 
         self.env = env
 
@@ -34,6 +36,7 @@ class Elo():
     def _close(self):
         self.model_shelf.close()
         self.result_shelf.close()
+        self.elo_value_shelf.close()
 
     def add_model(self, name, model_container):
         try:
@@ -74,8 +77,6 @@ class Elo():
         self.result_shelf[key] = total_results
 
     def _get_results(self, model_1, model_2, num_games=100):
-        # model_1_dict = self.model_shelf[model_1]
-        # model_2_dict = self.model_shelf[model_2]
 
         scheduler = self_play_parallel.SelfPlayScheduler(policy_container=self.model_shelf[model_1],
                                                          opposing_policy_container=self.model_shelf[model_2],
@@ -89,24 +90,28 @@ class Elo():
 
     def calculate_elo(self, anchor_model="random", anchor_elo=0):
         models = list(self.model_shelf.keys())
+
         model_indices = {model: i for i, model in enumerate(model for model in models if model != anchor_model)}
+        if "elo" in self.elo_value_shelf:
+            elo_values = self.elo_value_shelf["elo"]
+            initial_weights = [elo_values[model] for model in models if model != anchor_model]
+        else:
+            initial_weights = None
 
         self._convert_memory(model_indices)
 
-        # elos = torch.zeros(len(models) - 1, requires_grad=True)
         model_qs = {model: torch.ones(1, requires_grad=True) for model in models}  # q = 10^(rating/400)
         model_qs[anchor_model] = torch.tensor(10 ** (anchor_elo / 400), requires_grad=False)
         epoch_length = 1000
         num_epochs = 200
         batch_size = 32
-        elo_net = EloNetwork(len(models))
-        optim = torch.optim.SGD([elo_net.q_vals.weight], lr=800)
+        elo_net = EloNetwork(len(models), initial_weights)
+        optim = torch.optim.SGD([elo_net.elo_vals.weight], lr=800)
 
         for i in range(num_epochs):
             for j in range(epoch_length):
                 optim.zero_grad()
 
-                # result = self.memory.sample(1)[0]
                 batch = self.memory.sample(batch_size)
                 batch_t = result_container(*zip(*batch))
                 players, results = batch_t
@@ -115,25 +120,17 @@ class Elo():
                 expected_results = elo_net(players)
                 loss = elo_net.loss(expected_results, results)
 
-                # loss = self._calculate_loss(model_qs[result.p1], model_qs[result.p2], result.result)
                 loss.backward()
                 optim.step()
             for param_group in optim.param_groups:
                 param_group['lr'] = param_group['lr'] * 0.99
 
-        print(elo_net.q_vals.weight)
-        # model_elos = {model: torch.log10(q.data) * 400 for model, q in model_qs.items()}
-        model_elos = {model: elo_net.q_vals.weight.tolist()[0][model_indices[model]] for model in models if
+        model_elos = {model: elo_net.elo_vals.weight.tolist()[model_indices[model]] for model in models if
                       model != anchor_model}
         model_elos[anchor_model] = anchor_elo
+        self.elo_value_shelf["elo"] = model_elos
         print(model_elos)
         return model_elos
-
-    def _calculate_loss(self, q1, q2, result):
-        expected = q1 / (q1 + q2)
-        result_tensor = torch.tensor(result, requires_grad=False, dtype=torch.float)
-        loss = torch.nn.functional.l1_loss(expected, result_tensor)
-        return loss
 
     def _convert_memory(self, model_indices):
 
@@ -144,10 +141,6 @@ class Elo():
             val_1 = self._onehot(model1, model_indices)
             val_2 = self._onehot(model2, model_indices)
             players = torch.stack((val_1, val_2), 1).t()
-
-            # model_1_idx = model_indices[model1]
-            # model_2_idx = model_indices[model2]
-            # torch.nn.functional.one_hot(torch.tensor(model_1_idx, model_2_idx), len(model_indices) - 1)
 
             results = self.result_shelf[key]
             result_map = {"wins": 1, "losses": 0, "draws": 0.5}
@@ -166,22 +159,21 @@ class Elo():
 
 class EloNetwork(torch.nn.Module):
 
-    def __init__(self, num_models, *args, **kwargs):
+    def __init__(self, num_models, initial_weights=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.q_vals = torch.nn.Linear(num_models - 1, 1)  # q = 10^(rating/400)
+        self.elo_vals = torch.nn.Linear(num_models - 1, 1)  # q = 10^(rating/400)
         with torch.no_grad():
-            self.q_vals.bias.fill_(0.)
+            self.elo_vals.bias.fill_(0.)
+            if initial_weights:
+                self.elo_vals.weight.data = torch.FloatTensor(initial_weights)
+        self.elo_vals.requires_grad=True
 
     def forward(self, batch):
         batch = batch.float()
         batch1, batch2 = torch.split(batch, 1, 1)
-        r1 = self.q_vals.forward(batch1)
-        r2 = self.q_vals.forward(batch2)
+        r1 = self.elo_vals.forward(batch1)
+        r2 = self.elo_vals.forward(batch2)
 
-        # r = self.q_vals.forward(batch)
-        # q1 = q[0]
-        # q2 = q[1]
-        # r1, r2 = torch.split(r, 1, 1)
         q1 = torch.pow(10, r1 / 400)
         q2 = torch.pow(10, r2 / 400)
 
